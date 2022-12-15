74d3f04b5a374d4fd7dc988f00ea229d2b63f810
Ricky Valentine Oswald <ricky@primecarers.co.uk>
2022-12-15 23:31:21 +0000

    Added logging

--- strappy/views.py
+++ strappy/views.py
@@ -1,10 +1,15 @@
+import logging
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+logger = logging.getLogger(__name__)
 
 class Index(TemplateView):
     template_name = 'strappy/index.html'
 
     def get(self, request):
+        logger.warning('hmm')
         return render(
             request,
             self.template_name,
