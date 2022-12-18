e6a62de1d3975d272c713b4554167491b1de9bed
Ricky Valentine Oswald <ricky@primecarers.co.uk>
2022-12-18 11:45:28 +0000

    Added rockets to strappy page

--- strappy/views.py
+++ strappy/views.py
@@ -3,6 +3,8 @@ import logging
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from rockety.models import Rocket
+
 logger = logging.getLogger(__name__)
 
 class Index(TemplateView):
@@ -13,5 +15,7 @@ class Index(TemplateView):
         return render(
             request,
             self.template_name,
-            {},
+            {
+                'rockets': Rocket.objects.all(),
+            },
         )
